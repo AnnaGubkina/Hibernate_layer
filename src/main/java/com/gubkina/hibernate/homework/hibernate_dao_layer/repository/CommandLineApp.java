d82f3fd5e0ec29b204321cf7eb0bcded86e333a5
AnnaGubkina <gubkinatour@yandex>
2022-09-14 21:16:48 +0300

    migration-hibernate added

--- src/main/java/com/gubkina/hibernate/homework/hibernate_dao_layer/repository/CommandLineApp.java
+++ src/main/java/com/gubkina/hibernate/homework/hibernate_dao_layer/repository/CommandLineApp.java
@@ -17,36 +17,36 @@ import java.util.stream.IntStream;
  * This class adds new data and populates the table. To test our application
  */
 
-@Component
-public class CommandLineApp implements CommandLineRunner {
-
-    @PersistenceContext
-    EntityManager entityManager;
-
-    @Override
-    @Transactional
-    public void run(String... args) throws Exception {
-
-        List<String> cities = List.of("Moscow", "New York", "Bangkok", "Paris", "Dubai");
-        List<String> names = List.of("Anna", "Vasya", "Olga", "Tomas", "Leo", "Ilon", "Joe", "Grisha", "Gosha");
-        List<String> surnames = List.of("Twen", "Mask", "Grey", "Armstrong", "Kim", "Yang", "Listerman");
-        Random random = new Random();
-        IntStream.range(0, 100)
-                .forEach(i -> {
-                    Person person = Person.builder()
-                            .contact(Contact.builder()
-                                    .name(names.get(random.nextInt(names.size())))
-                                    .surname(surnames.get(random.nextInt(surnames.size())))
-                                    .age(random.nextInt(80))
-                                    .build())
-                            .city(cities.get(random.nextInt(cities.size())))
-                            .phoneNumber("222345" + i)
-                            .build();
-                    //сохраняем каждого человека в таблицу persons
-                    //System.out.println(person);
-                    //entityManager.persist(person);
-
-                });
-
-    }
-}
+//@Component
+//public class CommandLineApp implements CommandLineRunner {
+//
+//    @PersistenceContext
+//    EntityManager entityManager;
+//
+//    @Override
+//    @Transactional
+//    public void run(String... args) throws Exception {
+//
+//        List<String> cities = List.of("Moscow", "New York", "Bangkok", "Paris", "Dubai");
+//        List<String> names = List.of("Anna", "Vasya", "Olga", "Tomas", "Leo", "Ilon", "Joe", "Grisha", "Gosha");
+//        List<String> surnames = List.of("Twen", "Mask", "Grey", "Armstrong", "Kim", "Yang", "Listerman");
+//        Random random = new Random();
+//        IntStream.range(0, 100)
+//                .forEach(i -> {
+//                    Person person = Person.builder()
+//                            .contact(Contact.builder()
+//                                    .name(names.get(random.nextInt(names.size())))
+//                                    .surname(surnames.get(random.nextInt(surnames.size())))
+//                                    .age(random.nextInt(80))
+//                                    .build())
+//                            .city(cities.get(random.nextInt(cities.size())))
+//                            .phoneNumber("222345" + i)
+//                            .build();
+//                    //сохраняем каждого человека в таблицу persons
+//                    System.out.println(person);
+//                    entityManager.persist(person);
+//
+//                });
+//
+//    }
+//}
---
+++ src/main/resources/db/migration/V002__insert_into_persons.sql
@@ -0,0 +1,2 @@
+INSERT INTO hibernate_homework_db.persons (city, phone_number, name, surname, age)
+VALUES ('Bangkok', '89889483', 'Pavel', 'Pavlov', 23);
